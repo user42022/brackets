cb4ccea184fdcdf153967dcb520139ef6d294fd9
user42022 <andrekizilov@yandex.ru>
2023-01-16 22:07:36 +0300

    feat:implement a function returning boolean value if sequence of open-close chars given in a right order

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+      const strReduce = (str)=> {
+          let resStr=str
+          for (let pair of bracketsConfig) {
+              let regexp = new RegExp (pair.map((e,i,arr)=>e=(arr[i]==='|'||arr[i]==='('||arr[i]===')'||arr[i]==='['||arr[i]===']'||arr[i]==='{'||arr[i]==='}')?'\\'+arr[i]:arr[i]).join(''),'g')
+  
+              resStr=resStr.replace(regexp,'');
+          }
+      return resStr===str?resStr:strReduce(resStr)
+      }
+  return !strReduce(str).length
+  }
